92e34a8cb926e256cd3bf5f7c61fdf1d42962cb4
David Arutyunyan <arutiunian666@gmail.com>
2024-12-28 18:33:25 +0300

    refactor: simplify internationalization path matcher in middleware
    - Updated the matcher configuration in middleware.ts to streamline the exclusion of paths from internationalization, focusing on API and static files.
    - Enhanced comments for clarity, providing a concise overview of the paths excluded from the internationalization process.
    
    These changes improve the routing logic for internationalization, optimizing performance and maintainability.

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,21 +30,6 @@ const { auth } = NextAuth({
 export default auth((req) => intlMiddleware(req));
 
 export const config = {
-  matcher: [
-    /*
-     * Skip all paths that should not be internationalized
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - uploads (uploads folder)
-     * - lottie (lottie files)
-     * - logo.svg (logo file)
-     * - apple-icon.png (apple icon file)
-     * - icon.svg (icon file)
-     * - manifest.json (manifest file)
-     */
-    "/((?!api|_next/static|_next/image|uploads|lottie|favicon.ico|logo.svg|apple-icon.png|icon.svg|manifest.json).*)",
-  ],
+  // Skip all paths that should not be internationalized
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
 };
