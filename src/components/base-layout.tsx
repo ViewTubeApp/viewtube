6cd0d2467f2034d9f8ecdca4a9b8dfce634b909d
David Arutyunyan <arutiunian666@gmail.com>
2025-03-11 18:13:05 +0300

    refactor: restructure BaseLayout component provider hierarchy
    - Reordered provider components for better logical flow
    - Moved SessionProvider and TRPCReactProvider to outer scope
    - Simplified nested provider structure
    - Maintained existing component functionality

--- src/components/base-layout.tsx
+++ src/components/base-layout.tsx
@@ -4,13 +4,15 @@ import { TRPCReactProvider } from "@/trpc/react";
 import { HydrateClient } from "@/trpc/server";
 import { LanguageProvider } from "@inlang/paraglide-next";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Commissioner } from "next/font/google";
 import { MotionConfig } from "motion/react";
 import { SessionProvider } from "next-auth/react";
+import { Commissioner } from "next/font/google";
 import Head from "next/head";
 import { cookies } from "next/headers";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { type PropsWithChildren } from "react";
+
+import { cn } from "@/lib/utils";
 
 import { MOTION_DURATION } from "@/constants/motion";
 
@@ -39,35 +41,35 @@ export async function BaseLayout({ children, brand }: BaseLayoutProps) {
 
   return (
     <LanguageProvider>
-      <html lang={languageTag()} className={font.className} suppressHydrationWarning>
-        <Head>
-          <meta name="apple-mobile-web-app-title" content={brand} />
-        </Head>
-        <body>
-          <ThemeProvider attribute="class" defaultTheme="dark">
-            <SessionProvider>
-              <TRPCReactProvider>
-                <HydrateClient>
-                  <NuqsAdapter>
-                    <MotionConfig reducedMotion="user" transition={{ duration: MOTION_DURATION }}>
-                      <SidebarProvider defaultOpen={defaultOpen}>
+      <SessionProvider>
+        <TRPCReactProvider>
+          <HydrateClient>
+            <NuqsAdapter>
+              <MotionConfig reducedMotion="user" transition={{ duration: MOTION_DURATION }}>
+                <SidebarProvider defaultOpen={defaultOpen}>
+                  <html lang={languageTag()} className={font.className} suppressHydrationWarning>
+                    <Head>
+                      <meta name="apple-mobile-web-app-title" content={brand} />
+                    </Head>
+                    <body>
+                      <ThemeProvider attribute="class" defaultTheme="dark">
                         <ConsoleArt />
                         <AppSidebar collapsible="icon" />
                         <main className="w-full flex flex-col">
                           <Header />
                           <div className="relative p-2 sm:p-4 flex-1">{children}</div>
                         </main>
-                      </SidebarProvider>
-                    </MotionConfig>
-                  </NuqsAdapter>
-                  <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
-                </HydrateClient>
-              </TRPCReactProvider>
-            </SessionProvider>
-            <Toaster />
-          </ThemeProvider>
-        </body>
-      </html>
+                        <Toaster />
+                      </ThemeProvider>
+                    </body>
+                  </html>
+                </SidebarProvider>
+              </MotionConfig>
+            </NuqsAdapter>
+            <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+          </HydrateClient>
+        </TRPCReactProvider>
+      </SessionProvider>
     </LanguageProvider>
   );
 }
