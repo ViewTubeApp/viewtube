83c0b3229098ea868bee8526643f17001a6b8e77
David Arutyunyan <arutiunian666@gmail.com>
2025-03-15 00:11:12 +0300

    fix: add crossOrigin attribute to loadImage function
    - Added crossOrigin attribute to the Image object in loadImage function for handling CORS issues when loading images.
    - Ensured compatibility with external image sources.

--- src/utils/react/image.ts
+++ src/utils/react/image.ts
@@ -7,6 +7,7 @@
 export function loadImage(src: string) {
   return new Promise<HTMLImageElement>((resolve, reject) => {
     const img = new Image();
+    img.crossOrigin = "anonymous";
     img.onload = () => resolve(img);
     img.onerror = (...args) => reject(args);
     img.src = src;
