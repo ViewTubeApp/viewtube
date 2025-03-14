6bc2a325636b66e176f924c6660eaf796600c83b
David Arutyunyan <arutiunian666@gmail.com>
2025-03-15 00:58:51 +0300

    fix: enhance AmbientBackground component with aspect ratio handling
    - Added aspect ratio detection to adjust canvas dimensions based on image orientation.
    - Updated motion transitions for improved visual effects.
    - Refined canvas styling to maintain consistency across different aspect modes.

--- src/components/ambient-background.tsx
+++ src/components/ambient-background.tsx
@@ -2,6 +2,7 @@ import { loadImage } from "@/utils/react/image";
 import * as motion from "motion/react-client";
 import { useRef } from "react";
 import * as StackBlur from "stackblur-canvas";
+import { P, match } from "ts-pattern";
 
 import { motions } from "@/constants/motion";
 
@@ -13,10 +14,10 @@ export const AmbientBackground = ({ src }: AmbientBackgroundProps) => {
   const rootRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div ref={rootRef} className="absolute inset-0 brightness-50 -z-10 overflow-hidden">
+    <div ref={rootRef} className="absolute inset-0 -z-10 overflow-hidden">
       <motion.div className="absolute inset-0" {...motions.scale.reveal}>
         <canvas
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          className="absolute brightness-50 grayscale-25 opacity-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
           ref={(el) => {
             if (!el) {
               return;
@@ -26,10 +27,26 @@ export const AmbientBackground = ({ src }: AmbientBackgroundProps) => {
               .then((image) => {
                 // Max value is 254 for this algorithm
                 StackBlur.image(image, el, 254);
-                el.style.width = "100%";
-                el.style.height = "100%";
-                el.width = rootRef.current?.clientWidth ?? 0;
-                el.height = rootRef.current?.clientHeight ?? 0;
+                const aspectRatio = image.width / image.height;
+
+                // Determine the aspect mode of the image
+                const aspectMode = match(aspectRatio)
+                  .with(P.number.gt(1), () => "landscape")
+                  .with(P.number.lt(1), () => "portrait")
+                  .otherwise(() => "square");
+
+                // Set the aspect mode of the canvas
+                match(aspectMode)
+                  .with("landscape", () => {
+                    el.style.height = "100%";
+                  })
+                  .with("portrait", () => {
+                    el.style.width = "100%";
+                  })
+                  .otherwise(() => {
+                    el.style.width = "100%";
+                    el.style.height = "100%";
+                  });
               })
               .catch((err) => {
                 console.error(err);
