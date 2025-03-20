454cdf150a6ac626d87878daf32e3b84ba80a285
David Arutyunyan <arutiunian666@gmail.com>
2025-03-20 12:36:16 +0300

    chore: update Dockerfiles and configuration for improved clarity and functionality
    - Cleaned up Dockerfile.hermes by removing unnecessary comments and ensuring proper command formatting.
    - Simplified Dockerfile.nginx by removing redundant comments and maintaining essential configurations.
    - Refined Dockerfile.web by eliminating excessive comments and ensuring clarity in the build and serve stages.
    - Updated next.config.ts to remove redundant line breaks in comments for better readability.
    - Enhanced the GitHub Actions pipeline by adding a command to output the generated envfile, improving visibility during deployment.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful for Docker builds.
  */
 import BundleAnalyzer from "@next/bundle-analyzer";
 import { type NextConfig } from "next";
