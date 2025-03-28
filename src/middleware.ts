bd74828974d7bd31a1ae3f122b6c223231025552
David Arutyunyan <arutiunian666@gmail.com>
2025-03-28 23:57:58 +0300

    feat(middleware): enhance authentication middleware with route protection
    - Integrated `createRouteMatcher` to define protected routes for admin access.
    - Updated the `clerkMiddleware` to enforce authentication on protected routes.
    - Streamlined the middleware function to ensure proper handling of internationalization routing.
    - Adjusted the configuration to include root and locale routes for consistent middleware application.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,28 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import createMiddleware from "next-intl/middleware";
-import type { NextRequest } from "next/server";
 
 import { routing } from "./i18n/routing";
 import { checkAnonymousSession } from "./utils/server/session";
 
 const handleI18nRouting = createMiddleware(routing);
+const isProtectedRoute = createRouteMatcher(["/:locale/admin(.*)"]);
 
-export default clerkMiddleware((_, request) => {
-  return middleware(request);
-});
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
 
-async function middleware(request: NextRequest) {
   await checkAnonymousSession();
-  return handleI18nRouting(request);
-}
+  return handleI18nRouting(req);
+});
 
 export const config = {
   matcher: [
+    // Always run for root
+    "/",
+    // Always run for locale routes
+    "/(ru|en)/:path*",
+    // // Skip Next.js internals and all static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
   ],
 };
