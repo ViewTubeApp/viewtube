51d2e0efac7845cc1bb06bd37a737038b7b1ca01
David Arutyunyan <arutiunian666@gmail.com>
2025-04-03 18:18:48 +0300

    refactor(home-page): implement metadata generation for dynamic titles and descriptions
    - Added `generateMetadata` function to the home page, utilizing `getTranslations` for dynamic title and Open Graph metadata based on the locale.
    - This enhancement improves SEO and user experience by providing relevant metadata for the home page content.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,2 +1,28 @@
-export * from "./videos/page";
+import { type Metadata } from "next";
+import { type Locale } from "next-intl";
+import { getTranslations } from "next-intl/server";
+import { type SearchParams } from "nuqs/server";
+
+interface HomePageProps {
+  params: Promise<{ locale: Locale }>;
+  searchParams: Promise<SearchParams>;
+}
+
+export async function generateMetadata({ params }: HomePageProps) {
+  const { locale } = await params;
+
+  const t = await getTranslations({ locale });
+  const title = `${t("title_part_start")}${t("title_part_end")}`;
+
+  return {
+    title: t("videos"),
+    openGraph: {
+      title,
+      locale,
+      type: "website",
+      description: t("layout_description"),
+    },
+  } satisfies Metadata;
+}
+
 export { default } from "./videos/page";
