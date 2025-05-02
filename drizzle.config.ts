8865edb3263797a6e881344bcdb6111f8cf6aadc
David Arutyunyan <arutiunian666@gmail.com>
2025-05-02 20:37:42 +0300

    chore(config): switch database dialect from MySQL to PostgreSQL and comment out migration step in CI pipeline
    - Updated the database dialect in drizzle.config.ts to PostgreSQL for compatibility with the new database setup.
    - Commented out the migration step in the GitHub Actions pipeline to prevent execution during pull requests.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ debug("drizzle:config")(env);
 
 export default defineConfig({
   schema: "./src/server/db/schema",
-  dialect: "mysql",
+  dialect: "postgresql",
   dbCredentials: { url: env.DATABASE_URL },
   schemaFilter: ["public", "viewtube"],
 });
